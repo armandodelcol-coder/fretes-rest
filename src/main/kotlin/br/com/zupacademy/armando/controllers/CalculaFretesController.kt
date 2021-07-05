f68b07fd969da82475e6680aa6107fdbf128307b
Armando Tadeu <atdc.codemaster@gmail.com>
2021-07-05 19:57:54 -0300

    simple error handling

--- src/main/kotlin/br/com/zupacademy/armando/controllers/CalculaFretesController.kt
+++ src/main/kotlin/br/com/zupacademy/armando/controllers/CalculaFretesController.kt
@@ -1,10 +1,16 @@
 package br.com.zupacademy.armando.controllers
 
 import br.com.zupacademy.armando.CalculaFreteRequest
+import br.com.zupacademy.armando.ErrorDetails
 import br.com.zupacademy.armando.FretesServiceGrpc
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
 
 @Controller
 class CalculaFretesController(val grpcClient: FretesServiceGrpc.FretesServiceBlockingStub) {
@@ -13,8 +19,27 @@ class CalculaFretesController(val grpcClient: FretesServiceGrpc.FretesServiceBlo
         val request = CalculaFreteRequest.newBuilder()
             .setCep(cep)
             .build()
-        val response = grpcClient.calculaFrete(request)
-        return FreteResponse(cep = response.cep, valor = response.valor)
+        try {
+            val response = grpcClient.calculaFrete(request)
+            return FreteResponse(cep = response.cep, valor = response.valor)
+        } catch (e: StatusRuntimeException) {
+            val statusCode = e.status.code
+            val description = e.status.description
+            if (statusCode == Status.Code.INVALID_ARGUMENT) {
+                throw HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+
+            if (statusCode == Status.Code.PERMISSION_DENIED) {
+                val statusProto =
+                    StatusProto.fromThrowable(e) ?: throw HttpStatusException(HttpStatus.FORBIDDEN, description)
+
+                val anyDetails = statusProto.detailsList[0]
+                val errorDetails = anyDetails.unpack(ErrorDetails::class.java)
+                throw HttpStatusException(HttpStatus.FORBIDDEN, "${errorDetails.code}: ${errorDetails.message}")
+            }
+
+            throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.message)
+        }
     }
 }
 
